5ebaef8ba69e5c68a4dab9a10c88150c9e35a05d
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2019-12-22 17:55:55 +0100

    show weekday

--- src/apps/time.ts
+++ src/apps/time.ts
@@ -11,10 +11,22 @@ export class TimeApp implements App {
     show(): void {
         const time = dayjs().format('HH:mm');
 
+        // time
         this.controller.drawText({
             hexColor: '#00C8C8',
             text: time,
             position: { x: 7, y: 9 }
         });
+
+        // weekday
+        const weekday = dayjs().weekday();
+        const xPosition = weekday * 4 + 2;
+        console.log('weekday', weekday, xPosition);
+
+        this.controller.drawLine(
+            { x: xPosition, y: 7 },
+            { x: xPosition + 2, y: 7 },
+            '#00C8C8'
+        );
     }
 }
