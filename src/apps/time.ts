f3da4a18dd0e42854cb1844a23e881180e594efc
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2019-12-23 13:11:20 +0100

    show all weekdays and highlight the current one; refactoring

--- src/apps/time.ts
+++ src/apps/time.ts
@@ -9,24 +9,37 @@ export class TimeApp implements App {
     setup(): void {}
 
     show(): void {
+        this.controller.clear();
+
+        this.renderTime();
+        this.renderWeekday();
+
+        this.controller.show();
+    }
+
+    private renderTime(): void {
         const time = dayjs().format('HH:mm');
 
-        // time
         this.controller.drawText({
             hexColor: '#00C8C8',
             text: time,
             position: { x: 7, y: 9 }
         });
+    }
 
-        // weekday
-        const weekday = dayjs().weekday();
-        const xPosition = weekday * 4 + 2;
-        console.log('weekday', weekday, xPosition);
+    private renderWeekday(): void {
+        const currentWeekday = dayjs().weekday();
+        const getXPositionByWeekDay = (weekday: number) => weekday * 4 + 2;
 
-        this.controller.drawLine(
-            { x: xPosition, y: 7 },
-            { x: xPosition + 2, y: 7 },
-            '#00C8C8'
-        );
+        for (let weekday = 0; weekday < 7; weekday++) {
+            const xPosition = getXPositionByWeekDay(weekday);
+            const color = weekday === currentWeekday ? '#00C8C8' : '#A0A0A0';
+
+            this.controller.drawLine(
+                { x: xPosition, y: 7 },
+                { x: xPosition + 2, y: 7 },
+                color
+            );
+        }
     }
 }
