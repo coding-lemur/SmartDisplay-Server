5f6f45ae6c0ab9e55c8eeab00b426e77a10ca1d7
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2020-02-20 18:06:42 +0100

    add open-weather-service

---
+++ src/services/open-weather-map.service.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import { CityWeatherData } from '../models';
+
+export class OpenWeatherMapService {
+    constructor(private settings: any) {}
+
+    loadData(): Promise<CityWeatherData> {
+        const { settings } = this;
+
+        return new Promise((resolve, reject) => {
+            axios
+                .get('api.openweathermap.org/data/2.5/weather', {
+                    params: {
+                        id: settings.cityId,
+                        appid: settings.appId,
+                        units: settings.units
+                    }
+                })
+                .then(response => {
+                    const { data } = response;
+                    const result: CityWeatherData = {
+                        temperature: data.main.temp,
+                        humidity: data.main.humidity,
+                        windSpeed: data.wind.speed
+                    };
+                    resolve(result);
+                })
+                .catch(error => reject(error));
+        });
+    }
+}
