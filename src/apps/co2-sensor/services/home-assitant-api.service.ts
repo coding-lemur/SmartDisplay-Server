1300b346ebc8f6f76269994f41796511d235800b
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2021-04-10 17:34:28 +0200

    check co2 value is unavailable or too old

--- src/apps/co2-sensor/services/home-assitant-api.service.ts
+++ src/apps/co2-sensor/services/home-assitant-api.service.ts
@@ -1,7 +1,8 @@
 import fetch, { RequestInit } from 'node-fetch';
+import dayjs from 'dayjs';
 
 export class HomeAssistantApiService {
-    async loadCo2SensorValue(): Promise<number> {
+    async loadCo2SensorValue(): Promise<number | null> {
         const baseApiUrl = process.env.APP_CO2_SENSOR_BASE_API_URL;
         const entityId = process.env.APP_CO2_SENSOR_ENTITY_ID;
         const accessToken = process.env.APP_CO2_SENSOR_ACCESS_TOKEN;
@@ -17,7 +18,20 @@ export class HomeAssistantApiService {
         };
         const response = await fetch(url, options);
         const data = await response.json();
+        const { state, last_updated } = data;
 
-        return data.state;
+        if (state === 'unavailable') {
+            console.log('co2 value unavailable');
+            return null;
+        }
+
+        const diffMinutes = dayjs().diff(last_updated, 'minute');
+
+        if (diffMinutes >= 5) {
+            console.log('co2 value is too old', diffMinutes);
+            return null;
+        }
+
+        return state;
     }
 }
