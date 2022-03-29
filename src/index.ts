8ba90cfd6223643926b1ce0f9130977dcd9e4683
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2022-03-29 20:50:54 +0200

    optimize code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env ts-node
 
 import clear from 'clear';
-//import process from 'process';
-//import exitHook from 'exit-hook';
 import dayjs from 'dayjs';
 import weekday from 'dayjs/plugin/weekday';
 import 'dotenv/config';
@@ -12,28 +10,11 @@ import { Server } from './server';
 clear();
 console.log('SmartDisplay');
 
-let server: Server;
-
-/*process.on('SIGTERM', code => {
-    client.publish('awtrix-server', 'exit');
-    return console.log(`About to exit with code ${code}`);
-});*/
-
-//process.stdin.resume(); // so the program will not close instantly
-
-/*exitHook(() => {
-    console.debug('Exiting');
-
-    if (server != null) {
-        server.shutdown();
-    }
-});*/
-
 const locale = process.env.LOCALE;
 
 import(`dayjs/locale/${locale}`).then(() => {
     dayjs.locale(locale);
     dayjs.extend(weekday);
 
-    server = new Server();
+    const server = new Server();
 });
